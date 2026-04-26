d654664b89bf8fda40104174cbdffe88560d5ecf
shyamkumbhar1 <shyamkumbhar509@gmail.com>
2026-04-26 19:41:05 +0530

    add index

---
+++ database/migrations/2026_04_26_140652_add_indexes_to_scan_events_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('scan_events', function (Blueprint $table) {
+            $table->index('scan_id');
+            $table->index('action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('scan_events', function (Blueprint $table) {
+            $table->dropIndex(['scan_id']);
+            $table->dropIndex(['action']);
+        });
+    }
+};
