8d75dfad4f7b5de1c5e161a2f28a0d889fb1ddcd
shyamkumbhar1 <shyamkumbhar509@gmail.com>
2026-04-26 15:29:02 +0530

    create alert api

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -4,18 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ScanEvent;
+use App\Models\Alert;
 
 class ScanController extends Controller
 {
     public function ingest(Request $request)
-{
-    ScanEvent::create($request->all());
+    {
+        $exists = ScanEvent::where('scan_id', $request->scan_id)->exists();
+        if ($exists)
+         {
+            
+            Alert::create([
+                'scan_id' => $request->scan_id,
+                'type' => 'duplicate',
+                'message' => 'Duplicate scan detected'
+            ]);
+            return response()->json([
+                'success' => false,
+                'message' => 'Duplicate scan detected'
+            ], 200);
+        }
+        ScanEvent::create($request->all());
 
-    return response()->json([
-        'success' => true,
-        'message' => 'Scan stored successfully',
-        'data' => $request->all()
-        ], 200);
+        return response()->json([
+            'success' => true,
+            'message' => 'Scan stored successfully',
+            'data' => $request->all()
+            ], 200);
 
-}
+    }
 }
